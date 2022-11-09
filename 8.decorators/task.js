da9561082d79fda8cac246e4bd51d53e63db3b1a
BekkerV8 <valkenshtein@gmail.com>
2022-11-09 15:48:48 -0800

    Task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,54 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.find((item) => item.hash == hash);
+      if (objectInCache) {
+        console.log("Из кэша: " + objectInCache.value);
+        return "Из кэша: " + objectInCache.value;
+      }
+
+      let result = func(...args);
+      cache.push({
+        'hash': hash,
+        'value': result
+      });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
+  }
+
+
+  function debounceDecoratorNew(func, ms) {
+    let timer = null;
+
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+        clearTimeout(timer);
+        timer = setTimeout(() => func(...args), ms);
+      }
+    }
+    return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecorator2(func) {
+  let timer = null;
+
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+        clearTimeout(timer);
+        timer = setTimeout(() => func(...args), ms);
+      }
+      wrapper.count++;
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
