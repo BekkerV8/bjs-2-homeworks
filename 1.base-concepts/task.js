888364ea942390a0311eeeb408ba52735fcfc1ca
BekkerV8 <valkenshtein@gmail.com>
2022-11-09 14:09:18 -0800

    Task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr  = [];
+  let discriminant = b * b - 4 * a * c;
+  if (discriminant === 0) {
+     let x = -b / (2 * a);
+     arr.push(x);
+    }
+    else if (discriminant > 0) {
+      let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+      let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      arr.push(x1, x2);
+    };
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+//function calculateTotalMortgage(percent, contribution, amount, date) {
+//  let totalAmount;
+//
+//  // код для задачи №2 писать здесь
+//
+//  return totalAmount;
+//}
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
